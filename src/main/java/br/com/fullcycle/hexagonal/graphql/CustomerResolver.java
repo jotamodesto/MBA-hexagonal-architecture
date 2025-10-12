f4addc5aedc31ea89b9f1a9d888945def2d75214
Johnatan Modesto <johnatan.modesto@gmail.com>
2025-10-12 17:28:52 -0300

    Refactored Customer for a UseCase approach

--- src/main/java/br/com/fullcycle/hexagonal/graphql/CustomerResolver.java
+++ src/main/java/br/com/fullcycle/hexagonal/graphql/CustomerResolver.java
@@ -1,46 +1,39 @@
 package br.com.fullcycle.hexagonal.graphql;
 
+import br.com.fullcycle.hexagonal.application.exceptions.ValidationException;
+import br.com.fullcycle.hexagonal.application.usecases.CreateCustomerUseCase;
 import br.com.fullcycle.hexagonal.dtos.CustomerDTO;
 import br.com.fullcycle.hexagonal.models.Customer;
 import br.com.fullcycle.hexagonal.services.CustomerService;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.MutationMapping;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
+import java.net.URI;
 import java.util.Objects;
 
+// Resolver é um Adapter
 @Controller
 public class CustomerResolver {
     private final CustomerService customerService;
 
-    public CustomerResolver (final CustomerService customerService) {
+    public CustomerResolver(final CustomerService customerService) {
         this.customerService = Objects.requireNonNull(customerService);
     }
 
     @MutationMapping
-    public CustomerDTO createCustomer(@Argument CustomerDTO input) {
-        if (customerService.findByCpf(input.getCpf()).isPresent()) {
-            throw new RuntimeException("Customer already exists");
-        }
-        if (customerService.findByEmail(input.getEmail()).isPresent()) {
-            throw new RuntimeException("Customer already exists");
-        }
+    public CreateCustomerUseCase.Output createCustomer(@Argument CustomerDTO input) {
+        final var useCase = new CreateCustomerUseCase(customerService);
 
-        var customer = new Customer();
-        customer.setName(input.getName());
-        customer.setCpf(input.getCpf());
-        customer.setEmail(input.getEmail());
-
-        customer = customerService.save(customer);
-
-        return new CustomerDTO(customer);
+        return useCase.execute(new CreateCustomerUseCase.Input(input.getCpf(), input.getEmail(), input.getName()));
     }
 
     @QueryMapping
     public CustomerDTO customerOfId(@Argument Long id) {
-       return customerService.findById(id)
-               .map(CustomerDTO::new)
-               .orElse(null);
+        return customerService.findById(id)
+                .map(CustomerDTO::new)
+                .orElse(null);
     }
 }
